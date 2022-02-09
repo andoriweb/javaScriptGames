c4334dba17dedb217898004057c50db2f554a339
andoriweb <andoriweb@gmail.com>
2022-02-09 21:40:23 +0200

    word random and timer

--- guessTheWord/js/scripts.js
+++ guessTheWord/js/scripts.js
@@ -1,7 +1,22 @@
 // Список слов
-const wordList = [
+let wordList = [
   'собака', 'пианино', 'телевизор', 'тетрадь'
 ];
+
+let btnStart = document.querySelector('#btnStart'),
+    timer = null,
+    secondsLeft = 0,
+    strTimer = document.querySelector('#strTimer'),
+    strWord = document.querySelector('#strWord'),
+    currentWord = shuffle(wordList),
+    textResult = document.querySelector('#textResult'),
+    left = document.querySelector('#left'),
+    ttl = 5,
+    strLose = document.querySelector('#strLose'),
+    numWin = 0,
+    numLose = 0;
+  
+
 
 // Перемешивание массива слов
 // Тасование Фишера — Йетса
@@ -12,6 +27,45 @@ function shuffle(array) {
   }
 }
 
-console.log(wordList);
-shuffle(wordList);
-console.log(wordList);
+// Выводим новое слово
+function getNewWord() {
+  if (!wordList.length) {
+    left.innerHTML = 'Конец игры';
+    strWord.innerHTML = '';
+    textResult.value = '';
+    
+    return;
+  }
+  secondsLeft = ttl;
+  strTimer.innerHTML = secondsLeft;
+  let wordListShuffle = wordList.shift().split('');
+  shuffle(wordListShuffle);
+  strWord.innerHTML = wordListShuffle.join('');
+  textResult.focus();
+  textResult.value = '';
+  btnStart.disabled = 'disabled';
+  timer = setInterval(getTime, 1000);
+}
+
+// Провека вводимого слова
+function checkInput() {}
+
+// Остаток времени
+function getTime() {
+  secondsLeft--;
+  strTimer.innerHTML = secondsLeft;
+  if (secondsLeft == 0) {
+    numLose++;
+    strLose.innerHTML = numLose;
+    clearInterval(timer);
+    btnStart.disabled = '';
+  }
+}
+
+// Глобальный запуск
+window.onload = () => {
+  textResult.addEventListener('keyup', checkInput);
+  btnStart.addEventListener('click', getNewWord);
+  strTimer.innerHTML = secondsLeft;
+};
+ 
